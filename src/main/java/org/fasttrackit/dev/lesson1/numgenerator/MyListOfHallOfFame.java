bdc3da75ed659b88fd74e58fa6676bf927af42a9
Ionel Condor <icondor@sdl.com>
2016-01-30 19:45:22 +0200

    tech debts

--- src/main/java/org/fasttrackit/dev/lesson1/numgenerator/MyListOfHallOfFame.java
+++ src/main/java/org/fasttrackit/dev/lesson1/numgenerator/MyListOfHallOfFame.java
@@ -1,7 +1,6 @@
 package org.fasttrackit.dev.lesson1.numgenerator;
 
 
-
 import java.util.*;
 
 /**
@@ -11,50 +10,44 @@ import java.util.*;
 public class MyListOfHallOfFame {
 
     private static MyListOfHallOfFame m;
-
-    public static MyListOfHallOfFame getInstance() {
-
-        if(m==null) {
-            m=new MyListOfHallOfFame();
-            m.generateInitialList();
-        }
-       return m;
-    }
-
-
     private List<HallOfFame> hallOfFames = new ArrayList<HallOfFame>();
     private long id;
 
-    private void generateInitialList() {
+    public static MyListOfHallOfFame getInstance() {
+        if (m == null) {
+            m = new MyListOfHallOfFame();
+            m.generateInitialList();
+        }
+        return m;
+    }
 
-        for(int i=0;i<1000;i++) {
+    private void generateInitialList() {
+        for (int i = 0; i < 1000; i++) {
             hallOfFames.add(new HallOfFame(i, Math.abs(new Random().nextInt() % 20) + 3 + new Random().nextDouble()));
         }
-        id=1000;
+        id = 1000;
 
-    };
+    }
 
-   public long addItem(double value) {
-      id++;
-      hallOfFames.add(new HallOfFame(id, value));
-       return id;
-        }
+    public long addItem(double value) {
+        id++;
+        hallOfFames.add(new HallOfFame(id, value));
+        return id;
+    }
 
     public HallOfFame getMinScore() {
-        double min=Double.MAX_VALUE;
-        long lid=0;
+        double min = Double.MAX_VALUE;
+        long lid = 0;
         for (ListIterator<HallOfFame> iter = hallOfFames.listIterator(); iter.hasNext(); ) {
             HallOfFame element = iter.next();
 
-            if (element.getScore()<min) {
-                min=element.getScore();
-                lid=element.getId();
+            if (element.getScore() < min) {
+                min = element.getScore();
+                lid = element.getId();
             }
         }
-        return new HallOfFame(lid,min);
+        return new HallOfFame(lid, min);
     }
-
-
 
     public List getList() {
         return hallOfFames;
