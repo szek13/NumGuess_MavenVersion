c3259f762c9acd23a8fd571eb310da238561df96
Ionel Condor <icondor@sdl.com>
2016-01-30 20:29:19 +0200

    new test class

---
+++ src/test/java/org/fasttrackit/dev/lesson1/numgenerator/servlet/NumGenServletTest.java
@@ -0,0 +1,36 @@
+package org.fasttrackit.dev.lesson1.numgenerator.servlet;
+
+import org.fasttrackit.dev.lesson1.numgenerator.NumGeneratorBusinessLogic;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by icondor on 30/01/16.
+ */
+public class NumGenServletTest {
+
+    @Test
+    public void testBuildJSonObject() throws Exception {
+
+        NumGeneratorBusinessLogic nbl = new NumGeneratorBusinessLogic();
+
+        String result = NumGenServlet.buildJSonObject(nbl,"abc");
+        assertEquals(result,"{\"keyError\":\"WRONGNUMBERFORMAT\"}");
+
+
+    }
+
+
+    @Test
+    public void testnewBuildJSonObject() throws Exception {
+
+        NumGeneratorBusinessLogic nbl = new NumGeneratorBusinessLogic();
+
+
+        String result = NumGenServlet.buildJSonObject(nbl,"1");
+        assertEquals(result,"{\"keySuccess\":\"" + false + "\", \"keyHint\":\"" + "higher"+ "\", \"keyCounter\":\"" + "0.0" + "\", \"isMinimalScore\":\"" + false +"\", \"keyNrGuesses\":\"" + 1 + "\"}");
+
+
+    }
+}
