a52511c4a60ce8beba0c769cc8a3d2b1f3108425
Ionel Condor <icondor@sdl.com>
2016-01-30 21:00:59 +0200

    tech debt on javascript

--- src/main/webapp/js/numguessjs.js
+++ src/main/webapp/js/numguessjs.js
@@ -1,6 +1,6 @@
        function reset(){
             document.getElementById("serverResponse").innerText="";
-            xmlHttp = new XMLHttpRequest();
+            var xmlHttp = new XMLHttpRequest();
             xmlHttp.onreadystatechange=callback;
             var url ="NumGenServlet"+"?requestRestartGame=1";
             xmlHttp.open("GET",url,true);
@@ -8,7 +8,7 @@
         }
 
          function guess(){
-             xmlHttp = new XMLHttpRequest();
+             var xmlHttp = new XMLHttpRequest();
              xmlHttp.onreadystatechange=callback;
              var url ="NumGenServlet"+"?requestGuessNumber="+document.getElementById("number").value;
              xmlHttp.open("GET",url,true);
@@ -16,7 +16,7 @@
          }
 
         function guessLink(givenValue) {
-            xmlHttp = new XMLHttpRequest();
+            var xmlHttp = new XMLHttpRequest();
             xmlHttp.onreadystatechange=callback;
             var url ="NumGenServlet"+"?requestGuessNumber="+givenValue;
             xmlHttp.open("GET",url,true);
@@ -24,37 +24,32 @@
         }
 
          function callback() {
-             if(xmlHttp.readyState==4 && xmlHttp.status==200) {
+             if(xmlHttp.readyState===4 && xmlHttp.status===200) {
                  var jSonMessage = JSON.parse(xmlHttp.responseText);
                  var keyRestartGame=jSonMessage.keyRestartGame;
-                 if (keyRestartGame != undefined && keyRestartGame.length > 0) {
+                 if (keyRestartGame !== undefined && keyRestartGame.length > 0) {
                      alert("Restart cu succes, jocul a reinceput!");
                      document.getElementById("number").value="";
                      return;
                  }
 
-                 var keyError = jSonMessage.keyError;
-                 <!--if (keyError != undefined && keyError.length > 0) {-->
-                     <!--alert("Trebuie sa introduceti un numar valid!");-->
-                     <!--return;-->
-                 <!--}-->
-                var keySuccess = jSonMessage.keySuccess;
+                 var keySuccess = jSonMessage.keySuccess;
                  var keyHint = jSonMessage.keyHint;
                  var keyNrGuesses = jSonMessage.keyNrGuesses;
                  var keyCounter=jSonMessage.keyCounter;
                  var isMinimalScore=jSonMessage.isMinimalScore;
 
-                 if(keySuccess=="false") {
-                     if (keyHint == "higher")
+                 if(keySuccess==="false") {
+                     if (keyHint === "higher")
                          document.getElementById("serverResponse").innerHTML = "WRONG, Try a Higher one!";
-                     else if (keyHint == "lower")
+                     else if (keyHint === "lower")
                          document.getElementById("serverResponse").innerHTML = "WRONG, Try a Lower one!";
                  }
                  else
-                 if(keySuccess=="true")
+                 if(keySuccess==="true")
                  {
                      document.getElementById("serverResponse").innerHTML = "Congrats, you guessed the number " + document.getElementById("number").value + " after " + keyNrGuesses + " guesses."+"</br> Your time:"+keyCounter;
-                     if(isMinimalScore=="true")
+                     if(isMinimalScore==="true")
                       document.getElementById("serverResponse").innerHTML = "Congrats, you guessed the number " + document.getElementById("number").value + " after " + keyNrGuesses + " guesses."+"</br> You are now the first on this server with time:"+keyCounter;
 
 
